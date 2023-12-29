f5e001de90c0529c38f1c3311a5f7ac9fe853d0e
Kebe Liu <kebe.liu@daocloud.io>
2023-12-29 15:48:52 +0800

    + enhance response values for demo

--- Makefile
+++ Makefile
@@ -2,3 +2,16 @@ clean:
 	mvn clean
 package:
 	mvn package -DskipTests
+
+HUB ?= release-ci.daocloud.io/mspider
+VERSION ?= v0.2
+PLATFORM ?= linux/amd64 # linux/amd64,linux/arm64
+
+consumer-docker: package
+	cd dubbo-spring-boot-consumer; \
+	docker build -t $(HUB)/spring-dubbo3-consumer:$(VERSION) . --platform $(PLATFORM) --push
+provider-docker: package
+	cd dubbo-spring-boot-provider; \
+	docker build -t $(HUB)/spring-dubbo3-provider:$(VERSION) . --platform $(PLATFORM) --push
+
+push: consumer-docker provider-docker
--- dubbo-spring-boot-provider/src/main/java/io/daocloud/services/DemoServiceImpl.java
+++ dubbo-spring-boot-provider/src/main/java/io/daocloud/services/DemoServiceImpl.java
@@ -2,11 +2,29 @@ package io.daocloud.services;
 
 import io.daocloud.DemoService;
 import org.apache.dubbo.config.annotation.DubboService;
+import org.apache.dubbo.rpc.RpcContext;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.Map;
 
 @DubboService
 public class DemoServiceImpl implements DemoService {
     @Override
-    public String sayHello(String name) {
-        return "Hello " + name;
+    public HashMap<String, String> sayHello(String name) {
+        HashMap<String, String> res = new HashMap<>();
+        res.put("name", name);
+        res.put("rpc.url", RpcContext.getServerAttachment().getUrl().toString());
+        for (Map.Entry<String, Object> entry : RpcContext.getServerAttachment().getObjectAttachments().entrySet()) {
+            String key = entry.getKey();
+            Object value = entry.getValue();
+            // 处理每个附件的键值对
+            res.put("headers."+key, value.toString());
+        }
+        try {
+            res.put("hostname", InetAddress.getLocalHost().getHostName());
+        } catch (UnknownHostException ignored) {}
+        return res;
     }
 }
